456478994d83f02fafed5142531546aad77aedca
minju7346 <58619427+minju7346@users.noreply.github.com>
2021-02-07 22:31:32 +0900

    Backup

---
+++ GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public itemBox[] itemBoxes;
+    public bool isGameOver;
+    public GameObject winUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGameOver = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(0);
+        }
+        if (isGameOver == true)
+        {
+            return;
+        }
+        int count = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            if (itemBoxes[i].isOveraped == true)
+            {
+                count++;
+            }
+        }
+
+        if (count >= 3)
+        {
+            Debug.Log("게임 승리!!");
+            isGameOver = true;
+            winUI.SetActive(true);
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 10f;
+    public GameManager gameManager;
+    private Rigidbody playerRigidbody;
+
+    // 게임이 처음 시작되었을 때 한번 실행
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // 화면이 한번 깜빡일때 한번 실행
+    void Update()
+    {
+        if (gameManager.isGameOver == true)
+        {
+            return;
+        }
+        // -1 ~ +1
+        float inputX = Input.GetAxis("Horizontal");
+        float inputZ = Input.GetAxis("Vertical");
+
+        Vector3 velocity = new Vector3(inputX, 0, inputZ);
+        float fallSpeed = playerRigidbody.velocity.y;
+        velocity = velocity * speed;
+        velocity.y = fallSpeed;
+        playerRigidbody.velocity = velocity;
+
+
+
+        //발사 기능 - "FIre" - 마우스 왼쪽 버튼
+
+        //if(입력("Fire")
+        //         //총알을 발사 
+    }
+}
